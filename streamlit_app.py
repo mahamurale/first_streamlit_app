ca82f2f2bfd81691e99c5c6b0352b1cc6fa52d77
mahamurale <160591808+mahamurale@users.noreply.github.com>
2024-03-01 17:19:37 +0530

    breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New halthy Dinner')
+
+streamlit.title('Breakfast Menu')
+
+streamlit.content('Omega 3 & blueberry oatmeal')
+streamlit.content('kale, spinach & Rocket Smoothie ')
+streamlit.content('Hard Boiled Free Range Egg')
