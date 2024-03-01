9cfeb8b0dd6201485e334c3e5a048627ac629802
mahamurale <160591808+mahamurale@users.noreply.github.com>
2024-03-01 17:21:31 +0530

    BreakfastMenu-final

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My parents New halthy Dinner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.title('Omega 3 & blueberry oatmeal')
-streamlit.title('kale, spinach & Rocket Smoothie ')
-streamlit.title('Hard Boiled Free Range Egg')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('kale, spinach & Rocket Smoothie ')
+streamlit.text('Hard Boiled Free Range Egg')
